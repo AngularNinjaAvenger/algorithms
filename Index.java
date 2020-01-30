c2f7612613d4970149553750d4ae797c6dfaa1f2
osayi bishop <eguaborbishop1@gmail.com>
2020-01-30 08:49:54 +0100

    solved two sum algorithm 2

--- Index.java
+++ Index.java
@@ -7,10 +7,38 @@ import java.util.LinkedList;
 class Index {
 		public static void main(final String[] args) {
 			int[] list = new int[] {1,2,3,4,5};
-			int[] g = twoSum(list,7);
-			System.out.println(g[0]);
-			System.out.println(g[1]);
+			int target = 9;
+			int[] found = twoSum(list, target);
+			System.out.println(found[0]);
+			System.out.println(found[1]);
+		}
+		// TWO SUM TWO
+		/*
+			THIS IS USED IF THE THE ARRAY IS SORTED ELSE USE THE OTHER METHOD
+			WHEN YOU DOING ANY PROBLEM THAT HAS TO DO WITH SORTED ARRAYS USE A POINTER
+			note how this works, what it does is , it uses a pointer
+			so how this pointer works one will start from the begining 
+			of the array the other will start from ary.length 
+
 			
+		*/
+		public static int[] twosum_two(int[] lst,int idx){
+			int a_pointer = 0;
+			int b_pointer = lst.length - 1;
+			while(a_pointer < b_pointer){
+				/*
+					this will keep loop and adjusting the pointers
+					until it finds a match else it return null
+				*/
+				int sum = lst[a_pointer] + lst[b_pointer];
+
+				if(sum > idx)b_pointer--;
+				else if (sum < idx)a_pointer++;
+				// this is the base case that returns the array of the found items
+
+				else return new int[] {lst[a_pointer],lst[b_pointer]};
+			}
+			return new int[] {};
 		}
 		public static int[] sliceList(int[] ls,int idx){
 			int[] list = {};
