b5c07ee1cda590e08f3065e6ec8810292312e57a
osayi bishop <eguaborbishop1@gmail.com>
2020-03-10 17:13:54 +0100

    is valid

--- Index.java
+++ Index.java
@@ -32,28 +32,7 @@ class Index {
 			throw new IllegalArgumentException("can not be calculated");
 		}
 		
-		
-		public static boolean isValid(String parm){
-			Stack<Character> temp = new Stack<Character>();
-			for (int i = 0; i < parm.length(); i++) {
-				char strIdx = parm.charAt(i);
-				char l1 = '(';
-				char l2 = '{';
-				char l3 = '[';
-				char r1 = ')';
-				char r2 = '}';
-				char r3 = ']';
-				if(strIdx == l1 || strIdx == l2 || strIdx == l3){ 
-					temp.add(strIdx);
-				}else{
-					if(strIdx == r1 && temp.peek() == l1)temp.pop();
-					if(strIdx == r2 && temp.peek() == l2)temp.pop();
-					if(strIdx == r3 && temp.peek() == l3)temp.pop();
-				}
-			}
-			if(temp.empty())return true;
-			return false;
-		}
+	
 		// SINGLE NUMBER
 		/*
 			how you solve this problem is using the bitwise operator
---
+++ algorithms/sub-arrays/isvalid.java
@@ -0,0 +1,22 @@
+	
+		public static boolean isValid(String parm){
+			Stack<Character> temp = new Stack<Character>();
+			for (int i = 0; i < parm.length(); i++) {
+				char strIdx = parm.charAt(i);
+				char l1 = '(';
+				char l2 = '{';
+				char l3 = '[';
+				char r1 = ')';
+				char r2 = '}';
+				char r3 = ']';
+				if(strIdx == l1 || strIdx == l2 || strIdx == l3){ 
+					temp.add(strIdx);
+				}else{
+					if(strIdx == r1 && temp.peek() == l1)temp.pop();
+					if(strIdx == r2 && temp.peek() == l2)temp.pop();
+					if(strIdx == r3 && temp.peek() == l3)temp.pop();
+				}
+			}
+			if(temp.empty())return true;
+			return false;
+		}
