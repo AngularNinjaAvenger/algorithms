3b7f48e96113f48f02a85472938eb3459db490b1
osayi bishop <eguaborbishop1@gmail.com>
2020-03-10 17:31:08 +0100

    moved all algorithms to folder

--- Main.java
+++ Main.java
@@ -1,19 +1,6 @@
-/**
- * Main
- */
 public class Main {
 
     public static void main(final String[] args) {
 
     }
-    public static String removeVowel(String str){
-        return str.replaceAll("[aeiou]", "");
-    }
-   
-    
 }
-
-/**
- * InnerMain
- */
-
