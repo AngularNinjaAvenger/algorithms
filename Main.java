e6711c283a1559b0bdfcaceb4da49c30e6fd7ffd
osayi bishop <eguaborbishop1@gmail.com>
2020-03-10 17:25:44 +0100

    findingDuplicateNumber

--- Main.java
+++ Main.java
@@ -9,34 +9,7 @@ public class Main {
     public static String removeVowel(String str){
         return str.replaceAll("[aeiou]", "");
     }
-    public static int findDuplicateNumber(int[] list){
-        try{
-            int slow_pointer = 0;
-            int fast_pointer = 0;
-    
-            slow_pointer = list[slow_pointer];
-            fast_pointer = list[fast_pointer];
-            
-            while(slow_pointer < fast_pointer){
-                slow_pointer = list[slow_pointer];
-                fast_pointer = list[fast_pointer];
-            }
-            int a_pointer = slow_pointer;
-            int b_pointer = 0;
-    
-            a_pointer = list[a_pointer];
-            b_pointer = list[b_pointer];
-            
-            while(a_pointer != b_pointer){
-                a_pointer = list[a_pointer];
-                b_pointer = list[b_pointer];
-            }
-            return a_pointer;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //TODO: handle exception
-        }
-        return -1;
-    }
+   
     
 }
 
