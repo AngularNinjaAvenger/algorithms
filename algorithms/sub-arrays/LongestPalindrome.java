544c9660d869a3ac95d376505e919bf91c1ea3a8
osayi bishop <eguaborbishop1@gmail.com>
2020-03-10 17:19:32 +0100

    added LongestPalindrome

---
+++ algorithms/sub-arrays/LongestPalindrome.java
@@ -0,0 +1,18 @@
+public static int LongestPalindrome(String letter) {
+    int[] list = new int[128];
+    // WHAT WE ARE DOING HERE IS WITH THE CHAR
+    // WE CAN GET IT ASCIS INDEX IN THE 128 ARRY STORED ABOVE
+    // SO IF WE PASS SOMETHING LIKE c  WHICH IT'S 68 IT WILL
+    // INCREATE THE VALUE AT THAT IDX
+    for (char c : letter.toCharArray()) {
+        list[c]++;
+    }
+
+    int result = 0;
+    for(int i : list) {
+        result += i / 2 * 2;
+        if(result % 2 == 0 && i % 2 == 1)result+=1;
+
+    }
+    return result;
+}
