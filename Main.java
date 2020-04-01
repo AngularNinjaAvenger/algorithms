dd033954e398cb661d0bfc9210447332032911a9
AngularNinjaAvenger <osayibishop@gmail.com>
2020-04-01 20:27:27 +0100

    __

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(final String[] args) {
-        int[] new_list = findShortestSubArrayToSortAList(new int[] {1,4,3,2,5,8,9,21,10});
-        System.out.println(new_list);
+        
+    }
+    public static int[] findAllMissingNumber() {
+        int[] ms = new int[] {};
+        int min , int max;
+        int[] list = new int[] {1,2,3,4,5,6};
+        for (int i = 0; i < list.length; i++) {
+            min = Math.min(list[i],min); 
+            max = Math.max(list[i],max); 
+        }
+        int prev = max;
+        for (int j = list.length; j > 0; j--) {
+            for (int j2 = prev; j2 < list[j]; j2++) {
+                ms[ms.length < 1 ? 0 : ms.length-1 ] = prev+j2;
+            }
+        }
+
+        return null;
     }
     public static int[] findShortestSubArrayToSortAList(int[] list) {
         // this clones the list by defrencing the poitner
