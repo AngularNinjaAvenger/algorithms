ba66ef5ccee262c257f523c01e7ed04b01dd552e
AngularNinjaAvenger <osayibishop@gmail.com>
2020-04-13 14:54:12 +0100

    solving partionLabel

--- Main.java
+++ Main.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(final String[] args) {
-        
+
     }
+    /**
+     * now what this partitions label is doing is giving
+     * abcddacbd whatis the maxium patiions we can do
+     * and note each letter must be on it own pattion and 
+     * not occor in another one 
+     */
+    public List<Integer> partitionLabel(String s){
+        // this store the list of patitions
+        List<Integer> partitionLength = new ArrayList<>();
+        
+        
+        return null;
+    }
+
     public static int[][] list(int[][] list,int[] new_time) {
         int[] previous_time = list[0];
         for (int i = 0; i < list.length; i++) {
--- algorithms/algs/HouseRober.java
+++ algorithms/algs/HouseRober.java
@@ -1,4 +1,7 @@
 public static int houseRober(int house[]){
+    // this is solved using memoizaiton
+    // dynamic programming
+    
     int dp[] = new int[house.length+1];
     dp[0] = 0;
     dp[1] = house[1]; //<-- because it's not == zero
