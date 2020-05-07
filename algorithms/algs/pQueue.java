f888d8fc80f05f9e5efef7afd8e32e7a358b0556
AngularNinjaAvenger <osayibishop@gmail.com>
2020-05-07 14:34:34 +0100

    priority queue example

--- algorithms/algs/pQueue.java
+++ algorithms/algs/pQueue.java
@@ -4,7 +4,15 @@ import java.util.PriorityQueue;
 
 public class pQueue {
     
+    /**
+     * this is like a list but objects are added based 
+     * on a Comperator || a lambda expersion
+     */
     public static void main(String[] args) {
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(); 
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>((int num1,int num2) -> num1 > num2); 
+        queue.add(10);
+        queue.add(30);
+        queue.add(60);
+        System.out.println(queue.toString());
     }
 }
