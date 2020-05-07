621f8e9adf0913065c011777b6def7da615bcde0
AngularNinjaAvenger <osayibishop@gmail.com>
2020-05-07 14:28:04 +0100

    priority queue explanation

---
+++ algorithms/algs/pQueue.java
@@ -0,0 +1,10 @@
+package algorithms.algs;
+
+import java.util.PriorityQueue;
+
+public class pQueue {
+    
+    public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(); 
+    }
+}
