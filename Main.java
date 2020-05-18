1316d35fa7f24677f15011d47f6586033faf3f50
AngularNinjaAvenger <osayibishop@gmail.com>
2020-05-18 13:01:22 +0100

    peopleToSave immplementation

--- Main.java
+++ Main.java
@@ -15,8 +15,22 @@ public class Main {
      * given a list of ints your going to pair int to determine who can be saved
      * the goal is to get the minum number of list parts to save the int and each 
      * pir must not exceed a limit 
+     * 
+     * we re going to be using a 2 pointer to solve this
      */
     public int peopleToSave(int[] peopleToSave , int limit){
-        return null;
+        int numOfBoats = 0;
+        int i = 0;
+        int j = peopleToSave.length;
+        while(i <= j){
+            if(peopleToSave[i] + peopleToSave[j] <= limit){
+                j--;
+                i++;
+            }else{
+                j--;
+            }
+            numOfBoats++;
+        }
+        return numOfBoats;
     }
 }
